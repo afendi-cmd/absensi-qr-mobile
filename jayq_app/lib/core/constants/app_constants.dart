72ddb2f6f5f967b0d32df6729b9885d76c978900
basyirilahlicaniago <basyirilahlicaniago@gmail.com>
2026-06-24 18:10:40 +0700

    Tambah Perubahan

--- jayq_app/lib/core/constants/app_constants.dart
+++ jayq_app/lib/core/constants/app_constants.dart
@@ -27,7 +27,7 @@ class AppConstants {
   //    Backend harus jalan dengan: php artisan serve --host=0.0.0.0
   // static const String baseUrl =
   static const String baseUrl =
-      "http://192.168.1.8:8000/api"; // Ganti dengan IP Anda
+      "http://10.100.86.194:8000/api"; // Ganti dengan IP Anda
 
   // 4. Untuk iOS Simulator
   //    Gunakan saat: flutter run -d ios
